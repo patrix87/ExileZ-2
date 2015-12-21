bcbb49d7fa5e8a96a69d5ba56acbba26e572aba3
Patrix87 <patrix87@gmail.com>
2015-12-21 11:33:20 -0500

    .

--- exilez/config.cpp
+++ exilez/config.cpp
@@ -3,7 +3,10 @@ class CfgPatches {
 		units[] = {};
 		weapons[] = {};
 		requiredVersion = 0.1;
-		requiredAddons[] = {};
+		requiredAddons[] = {
+			"Ryanzombies",
+			"ryanzombiesfunctions"
+		};
 	};
 };
 
